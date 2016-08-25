1d9538153a5996706fdfd33d23ca6f0a9d7e41a7
ocerri <olmo.cerri@gmail.com>
2016-08-25 11:49:14 -0500

    Updated version with shrnking
    updated version

---
+++ Configuration/python/tower/SingleMuonFlatOneOverPt0p0005To0p5_tt25OC_cfi.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+# reference: https://github.com/cms-sw/genproductions/blob/master/python/EightTeV/SingleMuMinusFlatPt0p2To100_cff.py
+generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
+    PGunParameters = cms.PSet(
+        MaxOneOverPt = cms.double(0.5),
+        MinOneOverPt = cms.double(0.0005),
+        PartID = cms.vint32(13),
+        MaxEta = cms.double(1.),
+        MaxPhi = cms.double(2.0),
+        MinEta = cms.double(-0.4),
+        MinPhi = cms.double(0.4),
+        #XFlatSpread = cms.double(1.5),  ## in mm
+        #YFlatSpread = cms.double(1.5),  ## in mm
+        #ZFlatSpread = cms.double(150.), ## in mm
+        RandomCharge = cms.bool(True),
+        ReallyFlat = cms.bool(True),
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('single muon+/- 1/pt 0.0005 to 0.5 tower 27'),
+    AddAntiParticle = cms.bool(False),
+    firstRun = cms.untracked.uint32(1)
+)
